468683c5db0f8666fe5d04b45cc542a43e14faff
Michelle Lawson <lawson_m@soshgic.edu.gh>
2022-03-12 00:29:33 +0000

    second sanity

--- Sanity1.py
+++ Sanity1.py
@@ -1,10 +1,9 @@
 from alpha import *
 from alphaArrGen import *
-
+# 2D Array
 keys = [alphaArr,[A,a,B,b,C,c,D,d,E,e,F,f,G,g,H,h,I,i,J,j,K,k,L,l,M,m,N,n,O,o,P,p,Q,q,R,r,S,s,T,t,U,u,V,v,W,w,X,x,Y,y,Z,z,SPACE,period,comma,questionmark,at]]
 
 final_story = []
-temp_story = []
 get = input("Please input your story here: ")
 get = get.replace(" " ,"*") #for space " "
 content = list(get)
@@ -14,9 +13,7 @@ for letter in content:
 	for ind in range(len(keys[0])):
 		if letter == keys[0][ind]:
 			cell += 1
-			temp_story.append(keys[1][ind])
 			final_story.append((cell)*">"+keys[1][ind])
-
 
 story = ' '.join(str(e) for e in final_story)
 print("This is your story in BrainFuck "+"\n"+story)
